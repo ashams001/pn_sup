06da9c47a4c5f481227c400ccfae76bdd99b5bbc
vidyaiq <110374328+vidyaiq@users.noreply.github.com>
2023-08-02 14:29:07 +0530

    new file for invoice

--- admin_menu.php
+++ admin_menu.php
@@ -85,7 +85,7 @@
             </a>
         </li>
         <li class="nav-item menu-items">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="order_invoice.php">
               <span class="menu-icon">
                    <i class="fa-solid fa-file-lines"></i>
               </span>
