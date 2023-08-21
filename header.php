124c510525a1104ce26474b1e08c9b569281b560
vidyaiq <110374328+vidyaiq@users.noreply.github.com>
2023-08-21 09:35:58 +0530

    added cdn in javascript link

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <link rel="stylesheet" href="assets/css/select2-bootstrap.min.css">
 <link href="assets/css/Roboto.css" rel="stylesheet"
       type="text/css">
-<link rel="stylesheet" href="assets/fonts/fontawesome/css/all.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"/>
 <!-- End plugin css for this page -->
 <!-- inject:css -->
 <!-- endinject -->
